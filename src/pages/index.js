cc322314cd82b2f78ce2d8cbeb0279f2186d60c4
Jae Kim <79775075+wodusdlxx@users.noreply.github.com>
2023-09-26 23:51:22 -0400

    I am making changes to my website

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,25 @@
-//import styles from './page.module.css'
 import Data from "../app/components/data";
 import ArticleCard from "../app/components/ArticleCard";
-import Article from "./article";
+import styles from "../app/page.module.css";
 
 export default function Home() {
   const projectName = "Exercise Two";
 
   return (
-    <main>
+    <main className={styles.page}>
         <h1>{projectName}</h1>
-        <ArticleCard title = "Cool Title" description = "Cool description" />
-        <ArticleCard title = "Another Title" />
-    </main>
+        {Data.map((article) => (
+          <ArticleCard
+          key={article.id}
+          date={new Date(article.publishedDate).toDateString()}
+          description={article.blurb}
+          id={article.id}
+          imageAlt={article.image.alt}
+          imageSrc={"https://images.squarespace-cdn.com/content/v1/607cc34ebab4b20ba47a3b97/b62f442c-a165-4059-b90d-a083097d567a/Old-Money-Aesthetic-Style-Guide-2023?format=2500w"}
+          title={article.title}
+        />
+        ))}
+      
+      </main>
   );
 }
